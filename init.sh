5ae3deab2b1f6f5f7a6df7c5ef0fdfdfa9e99e21
TEVA <vanquish.ld@gmail.com>
2014-06-02 04:49:24 +0900

    second commit!

--- init.sh
+++ init.sh
@@ -17,18 +17,21 @@ fi
 
 cd $DOTFILES
 
-# git submodule update --init
+git submodule update --init
 
-ln -s $DOTFILES/.zshrc $HOME/.zshrc
-ln -s $DOTFILES/.bash_history $HOME/.bash_history
-ln -s $DOTFILES/.bash_logout $HOME/.bash_logout
-ln -s $DOTFILES/.bash_profile $HOME/.bash_profile
-ln -s $DOTFILES/.bashrc $HOME/.bashrc
-ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
-ln -s $DOTFILES/inputrc $HOME/inputrc
-ln -s $DOTFILES/.vimrc $HOME/.vimrc
-ln -s $DOTFILES/.zsh_history $HOME/.zsh_history
+ln -s $DOTFILES/zshrc $HOME/.zshrc
+ln -s $DOTFILES/zsh_history $HOME/.zsh_history
+#
+ln -s $DOTFILES/bashrc $HOME/.bashrc
+ln -s $DOTFILES/bash_profile $HOME/.bash_profile
+ln -s $DOTFILES/bash_logout $HOME/.bash_logout
+ln -s $DOTFILES/bash_history $HOME/.bash_history
+#
+ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+#
+ln -s $DOTFILES/inputrc $HOME/.inputrc
+ln -s $DOTFILES/vimrc $HOME/.vimrc
 
-# vim -c ':BundleInstall!'
+vim -c ':BundleInstall!'
 
 
