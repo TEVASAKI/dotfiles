d7e903cbaa03b6244dd84688a3f491244ea001e5
TEVA <vanquish.ld@gmail.com>
2014-07-10 23:24:39 +0900

    History files REJECTED on this shell.

--- move.sh
+++ move.sh
@@ -3,12 +3,10 @@
 DOTFILES="$HOME/Dropbox/dotfiles"
 
 mv $HOME/.zshrc $DOTFILES/zshrc
-mv $HOME/.zsh_history $DOTFILES/zsh_history
 #
 mv $HOME/.bashrc $DOTFILES/bashrc
 mv $HOME/.bash_profile $DOTFILES/bash_profile
 mv $HOME/.bash_logout $DOTFILES/bash_logout
-mv $HOME/.bash_history $DOTFILES/bash_history
 #
 mv $HOME/.gitconfig $DOTFILES/gitconfig
 #
@@ -17,12 +15,10 @@ mv $HOME/.vimrc $DOTFILES/vimrc
 
 
 ln -s $DOTFILES/zshrc $HOME/.zshrc
-ln -s $DOTFILES/zsh_history $HOME/.zsh_history
 #
 ln -s $DOTFILES/bashrc $HOME/.bashrc
 ln -s $DOTFILES/bash_profile $HOME/.bash_profile
 ln -s $DOTFILES/bash_logout $HOME/.bash_logout
-ln -s $DOTFILES/bash_history $HOME/.bash_history
 #
 ln -s $DOTFILES/gitconfig $HOME/.gitconfig
 #
