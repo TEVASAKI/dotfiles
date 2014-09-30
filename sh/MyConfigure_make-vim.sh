aea4e4ed381a90b7becc787729615a0214ecc557
TEVA <vanquish.ld@gmail.com>
2014-09-30 13:54:50 +0900

    Del perl option :{

--- sh/MyConfigure_make-vim.sh
+++ sh/MyConfigure_make-vim.sh
@@ -7,7 +7,7 @@ source ./CHECK.sh
 
 CHECK hg pull
 CHECK hg update
-CHECK ./configure --enable-multibyte --with-features=huge --disable-selinux --prefix=/usr/local --srcdir=. --srcdir=. --enable-gui=auto --enable-python3interp=dynamic --enable-pythoninterp=dynamic --enable-perlinterp=dynamic
+CHECK ./configure --enable-multibyte --with-features=huge --disable-selinux --prefix=/usr/local --srcdir=. --srcdir=. --enable-gui=auto --enable-python3interp=dynamic --enable-pythoninterp=dynamic
 CHECK make clean 
 CHECK make -j 4 
 #su -
