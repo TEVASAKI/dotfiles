3335d434c25cec26229be9df36f951f16789c942
TEVASAKI- <vanquish.ld+github@gmail.com>
2016-02-01 03:58:33 +0900

    new file:   git-bootstrap.sh
    Add Git config init shell

---
+++ git-bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git config --global user.name "TEVASAKI-"
+git config --global user.email "vanquish.ld+github@gmail.com"
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.grep auto
+git config --global color.branch auto
+git config --global push.default current
+git config --global core.precomposeunicode true
+git config --global core.quotepath false
+
